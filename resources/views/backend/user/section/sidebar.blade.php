4d712f7f84b5ededac4a9c69a725139c43e458b5
SAK OUSA <hjhfg7226@gmail.com>
2025-11-17 21:32:03 +0700

    Add user dasboard

--- resources/views/backend/user/section/sidebar.blade.php
+++ resources/views/backend/user/section/sidebar.blade.php
@@ -14,7 +14,7 @@
 
     <li class="">
 
-        <a href="#"><svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24"
+        <a href=""><svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24"
                 width="18px">
                 <path d="M0 0h24v24H0V0z" fill="none" />
                 <path
@@ -42,7 +42,6 @@
             </svg> Wishlist
         </a>
     </li>
-
 
 
     <li class="">
